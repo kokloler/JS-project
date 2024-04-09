af14244e4fa25903c63b26ffd286f3ee269f6e86
kokloler <jelte.kessels@gmail.com>
2024-04-09 14:48:55 +0200

    yay
    hihihhiaha

---
+++ HTML/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="..//css/imports.css">
+    <title>homepage</title>
+</head>
+
+<body>
+    <h1>Afdekplaat</h1>
+    <div class="black progressBar">
+        <p> progressie 0/20 </p>
+    </div>
+    <h2>Vraag Lorem impsum </h2>
+    <label for="answer"> Vul in</label>
+    <input type="text" id="answer">
+</body>
+
+</html>
